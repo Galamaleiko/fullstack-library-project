3ef251742caef50719330856aa00179382f2df7b
Serhii <sergijba@gmail.com>
2025-01-17 17:21:19 +0100

    Project Init

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import BookOverview from "@/components/BookOverview";
+import { Button } from "@/components/ui/button";
+
+const Home = () => (
+  <>
+    <BookOverview />
+  </>
+);
+
+export default Home;
