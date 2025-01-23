060168bee0e1b836855297a9e6f3db999a3d2681
Serhii <sergijba@gmail.com>
2025-01-23 20:30:49 +0100

    send email fix #3

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -20,8 +20,8 @@ export const sendEmail = async ({
   message: string;
   to_email: string;
 }) => {
-  emailjs
-    .send(
+  try {
+    const response = await emailjs.send(
       config.env.emailjs.serviceId,
       config.env.emailjs.templateId,
       {
@@ -32,13 +32,16 @@ export const sendEmail = async ({
         to_email,
       },
       config.env.emailjs.publicKey
-    )
-    .then(
-      () => {
-        console.log("Success");
-      },
-      (error) => {
-        console.log("Error on API call! ", error.text);
-      }
     );
+
+    console.log("Email sent successfully", response);
+  } catch (error) {
+    console.log("Error on API call! ", error);
+
+    if (error instanceof Error) {
+      console.error("Erorr details:", error.message);
+    } else {
+      console.error("Unknown error:", error);
+    }
+  }
 };
