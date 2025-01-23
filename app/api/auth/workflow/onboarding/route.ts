d4041f29d95c25ea7f67372c88a03a9dd1b573b0
Serhii <sergijba@gmail.com>
2025-01-23 19:40:23 +0100

    Emailjs integration

--- app/api/auth/workflow/onboarding/route.ts
+++ app/api/auth/workflow/onboarding/route.ts
@@ -1,45 +1,83 @@
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { sendEmail } from "@/lib/workflow";
 import { serve } from "@upstash/workflow/nextjs";
+import { eq } from "drizzle-orm";
+
+type UserState = "non-active" | "active";
 
 type InitialData = {
   email: string;
+  fullName: string;
+};
+
+const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
+const THREE_DAY_IN_MS = 3 * ONE_DAY_IN_MS;
+const ONE_MONTH_IN_MS = 30 * ONE_DAY_IN_MS;
+
+const getUserState = async (email: string): Promise<UserState> => {
+  const user = await db
+    .select()
+    .from(users)
+    .where(eq(users.email, email))
+    .limit(1);
+
+  if (user.length === 0) return "non-active";
+
+  const lastActivityDate = new Date(user[0].lastActivityDate!);
+  const now = new Date();
+  const timeDiff = now.getTime() - lastActivityDate.getTime();
+
+  if (timeDiff > THREE_DAY_IN_MS && timeDiff <= ONE_MONTH_IN_MS) {
+    return "non-active";
+  }
+
+  return "active";
 };
 
 export const { POST } = serve<InitialData>(async (context) => {
-  const { email } = context.requestPayload;
+  const { email, fullName } = context.requestPayload;
 
-  await context.run("new-signup", async () => {
-    await sendEmail("Welcome to the platform", email);
+  // Welcome Email
+  await context.run("welcome-email", async () => {
+    await sendEmail({
+      subject: "Welcome!",
+      from_name: "University Library",
+      to_name: fullName,
+      to_email: email,
+      message: `<p>Hello, ${fullName},</p><br>Welcome to our website, the University Library platform!`,
+    });
   });
 
   await context.sleep("wait-for-3-days", 60 * 60 * 24 * 3);
 
   while (true) {
     const state = await context.run("check-user-state", async () => {
-      return await getUserState();
+      return await getUserState(email);
     });
 
     if (state === "non-active") {
       await context.run("send-email-non-active", async () => {
-        await sendEmail("Email to non-active users", email);
+        await sendEmail({
+          subject: "Are you still there?",
+          from_name: "University Library",
+          to_name: fullName,
+          to_email: email,
+          message: `Hey, ${fullName}, we miss you!`,
+        });
       });
     } else if (state === "active") {
       await context.run("send-email-active", async () => {
-        await sendEmail("Send newsletter to active users", email);
+        await sendEmail({
+          subject: "Welcome back!",
+          from_name: "University Library",
+          to_name: fullName,
+          to_email: email,
+          message: `It's good to see you active, ${fullName}!`,
+        });
       });
     }
 
     await context.sleep("wait-for-1-month", 60 * 60 * 24 * 30);
   }
 });
-
-async function sendEmail(message: string, email: string) {
-  // Implement email sending logic here
-  console.log(`Sending ${message} email to ${email}`);
-}
-
-type UserState = "non-active" | "active";
-
-const getUserState = async (): Promise<UserState> => {
-  // Implement user state logic here
-  return "non-active";
-};
--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,7 +1,39 @@
 import { Client as WorkflowClient } from "@upstash/workflow";
 import config from "./config";
+import emailjs from "@emailjs/browser";
 
 export const workflowClient = new WorkflowClient({
   baseUrl: config.env.upstash.qstashUrl,
   token: config.env.upstash.qstashToken,
 });
+
+export const sendEmail = async ({
+  subject,
+  from_name = "University Library",
+  to_name,
+  message,
+  to_email,
+}: {
+  subject: string;
+  from_name: string;
+  to_name: string;
+  message: string;
+  to_email: string;
+}) => {
+  await emailjs
+    .send(config.env.emailjs.serviceId, config.env.emailjs.templateId, {
+      subject,
+      from_name,
+      to_name,
+      message,
+      to_email,
+    })
+    .then(
+      () => {
+        console.log("Success");
+      },
+      (error) => {
+        console.log("Error! ", error.text);
+      }
+    );
+};
